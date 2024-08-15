b46e78258396521f914980f03b5cf900e2a3421b
varun-muthanna <97382175+varun-muthanna@users.noreply.github.com>
2024-08-15 18:53:22 +0530

    grpc

--- working/main.go
+++ working/main.go
@@ -8,21 +8,50 @@ import (
 	"os/signal"
 	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/varun-muthanna/handlers"
+	protos "github.com/varun-muthanna/products-api/currency"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
 
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 
-	hh := handlers.NewProduct(l)
+	conn, err := grpc.Dial("localhost:9093", grpc.WithTransportCredentials(insecure.NewCredentials()))
 
-	sm := http.NewServeMux() // default HandleFunc calls ServeMux which is implemented by ServeHTTP
-	sm.Handle("/", hh)
+	if err != nil {
+		panic(err)
+	}
+
+	defer conn.Close()
+
+	currencyclient := protos.NewCurrencyClient(conn)
+
+	hh := handlers.NewProduct(l, currencyclient)
+
+	m := mux.NewRouter()
+
+	getrouter := m.Methods("GET").Subrouter() //only GET
+
+	getrouter.HandleFunc("/{id:[0-9]+}", hh.ConvertProducts)
+	getrouter.HandleFunc("/", hh.GetProducts)
+
+	putrouter := m.Methods("PUT").Subrouter()
+	putrouter.HandleFunc("/{id:[0-9]+}", hh.UpdateProducts)
+	putrouter.Use(hh.MiddlewareValidation)
+
+	postrouter := m.Methods("POST").Subrouter()
+	postrouter.HandleFunc("/", hh.AddProducts)
+	postrouter.Use(hh.MiddlewareValidation)
+
+	//sm := http.NewServeMux() // default HandleFunc calls ServeMux which is implemented by ServeHTTP
+	//sm.Handle("/", hh)
 
 	s := &http.Server{
 		Addr:         ":8000",           // configure the bind address
-		Handler:      sm,                // set the default handler
+		Handler:      m,                 // set the default handler
 		ErrorLog:     l,                 // set the logger for the server
 		ReadTimeout:  5 * time.Second,   // max time to read request from the client
 		WriteTimeout: 10 * time.Second,  // max time to write response to the client
